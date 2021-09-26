68f31f9c57547715331b4526c0ae066217ff3cc1
Rendezvoused <89423721+Rendezvoused@users.noreply.github.com>
2021-09-26 15:17:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js"></script>
+<script src="https://kit.fontawesome.com/860e19f8d2.js"></script><style media="all" id="fa-main">
+<script src="//pull.cappuccicons.com/cpf.js"></script><link id="myCss" rel="stylesheet" type="text/css" href="//icons.cappuccicons.com/cpf.css" media="all">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&amp;display=swap" rel="stylesheet">
+<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Shrikhand&amp;display=swap" rel="stylesheet">
+<script src="https://static.tumblr.com/iuw14ew/VSQma1786/jquery.style-my-tooltips.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Libre+Barcode+128&amp;display=swap" rel="stylesheet">
+
+<script>
+  <script>
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "380px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}    
+</script>
+
+
+<script>
+
+(function($){
+
+$(document).ready(function(){
+
+$("[title]").style_my_tooltips({
+
+tip_follows_cursor:true,
+
+tip_delay_time:90,
+
+tip_fade_speed:600,
+
+attribute:"title"
+
+});
+
+});
+ 
+})(jQuery);
+
+</script> 
+ <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#draggable" ).draggable();
+  } );
+  </script>
+/*! scripts/tumblelog_post_message_queue.js */
+!function(e){"use strict";e.postMessageQueue||(e.postMessageQueue=[],e.postMessageCallback=function(s){e.postMessageQueue.push(s.data)},window.addEventListener?window.addEventListener("message",e.postMessageCallback):window.attachEvent("onmessage",e.postMessageCallback))}(window.Tumblr||(window.Tumblr={}));
+
+</script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  #draggable { width: auto; height: auto; border: none;}
+  </style>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $( function() {
+    $( "#draggable" ).draggable();
+  } );
+  </script>
